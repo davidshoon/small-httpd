6296a700afbd19da46ab572b34813821865ec3dd
David Shoon <18284887+davidshoon@users.noreply.github.com>
2020-07-17 07:13:19 +1000

    Refactored code and added comments for each function.
    Also made some better logging (and removed 1 useless log message).
    
    Removed "no fork" version, as OpenBSD runs fine with forking (it was some other error that caused it to play up).

--- src/test.c
+++ src/test.c
@@ -1,5 +1,5 @@
-/* 
-	Small HTTP daemon. 
+/*
+	Small HTTP daemon.
 
 	Based off "Example TCP server" in "icebox/originals"
 
@@ -9,7 +9,7 @@
 
 	This program chroots and drops privileges (setuid()) after listening to the port.
 
-	Also has "stack canary" protection, useful for platforms that don't have this. 
+	Also has "stack canary" protection, useful for platforms that don't have this.
 	This should prevent stack buffer overflow attacks.
 
 	This program avoids using the heap. If you want to change this code to using heap (malloc, etc)
@@ -17,32 +17,31 @@
 	it triggers a SEGFAULT signal when overrun.
 */
 
+/*========================== SECURITY SETTINGS -- YOU SHOULDN'T NEED TO CHANGE THIS =======================*/
+
 /* Set chroot to current directory, otherwise must chdir() to directory as well (not done) */
 #define CHROOT_DIR "."
 
 /* Setuid to nobody */
 #define SETUID_TO_USER "nobody"
 
+/* Our own canary protection, for compilers that don't have this available. */
+#define CANARY_PROTECTION
+
+
+/*========================== DEFAULT SPLIT STRING LIMITS =======================*/
+
+/* string buffer size -- i.e. per split word */
 #define BUFFER_SIZE 1024
 
+/* maximum number of words (tokens) returned from split() */
 #define MAX_SPLITS 100
+
+/*========================== LOGGING MACROS =======================*/
 
 #define LOGERR(x, ...) printf("ERROR: " x, ##__VA_ARGS__)
 #define LOGINFO(x, ...) printf("INFO: " x, ##__VA_ARGS__)
 #define LOG printf
-
-#define CANARY_PROTECTION
-
-#ifdef CANARY_PROTECTION
-int main_canary = 0;
-	#define ENTER int canary = main_canary
-	#define RETURN(x) if (canary == main_canary) return x; else abort()
-#else
-	#define ENTER
-	#define RETURN(x) return x
-#endif
-
-// #define NO_FORK
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -69,11 +68,23 @@ int main_canary = 0;
 
 #include <time.h>
 
+
+#ifdef CANARY_PROTECTION
+int main_canary = 0;
+	#define ENTER int canary = main_canary
+	#define RETURN(x) if (canary == main_canary) return x; else abort()
+#else
+	#define ENTER
+	#define RETURN(x) return x
+#endif
+
 struct split_string
 {
 	char str[BUFFER_SIZE];
 };
 
+
+// my_strlcpy - like OpenBSD's strlcpy(), but slightly different. Doesn't return strlen(src).
 void my_strlcpy(char *dest, const char *src, int len)
 {
 	ENTER;
@@ -94,6 +105,8 @@ void my_strlcpy(char *dest, const char *src, int len)
 	/* We don't return strlen(src), cause we want to take extra precautions against unterminated src fields, and we don't care about truncation. */
 }
 
+// split - splits a "src" string by delimiter "delim", with destination "dst" and total number of "split words" defined by "split_string_size".
+// returns number of "split words"
 int split(char *src, char *delim, struct split_string *dst, int split_string_size)
 {
 	ENTER;
@@ -117,7 +130,7 @@ int split(char *src, char *delim, struct split_string *dst, int split_string_siz
 		else {
 			my_strlcpy(dst[i].str, p, strlen(p) + 1); // copy till the end of the string p, since it's the final round. (+1 for null terminator)
 			LOGINFO("Final split: [%s]\n", dst[i].str);
-			i++;
+			i++; // increment so that we have the total "split_string_size" before breaking out of this loop
 			break;
 		}
 	}
@@ -125,7 +138,8 @@ int split(char *src, char *delim, struct split_string *dst, int split_string_siz
 	RETURN(i);
 }
 
-
+// strip_newline -- strips newline character "\r\n" from string "s".
+// Returns string "s".
 char *strip_newline(char *s)
 {
 	ENTER;
@@ -134,20 +148,73 @@ char *strip_newline(char *s)
 	RETURN(s);
 }
 
+// http_send_file -- sends a file named "filename", to "fp_out" (which is presumably a socket).
+// Returns number of bytes sent.
+int http_send_file(int http_version, const char *filename, FILE *fp_out)
+{
+	ENTER;
+	FILE *fp;
+	int total_size;
+	char readbuf[1024];
+	int r, s;
+	struct stat statbuf;
+
+	fp = fopen(filename, "rb");
+	if (!fp) {
+		LOGERR("File not found: %s\n", filename);
+		fprintf(fp_out, "HTTP/1.%d 404 Not Found\n", http_version);
+		fprintf(fp_out, "Content-Type: text/html; charset=utf-8\n\n");
+		fprintf(fp_out, "<HTML><BODY>File not found</BODY></HTML>\n");
+		fflush(fp_out);
+		RETURN(-1);
+	}
+
+	else {
+		// we want to follow symlinks... so use stat() instead of lstat()
+		stat(filename, &statbuf);
+
+		fprintf(fp_out, "HTTP/1.%d 200 OK\n", http_version);
+		fprintf(fp_out, "Content-Length: %ld\n\n", statbuf.st_size);
+
+		total_size = 0;
+
+		do {
+			r = fread(readbuf, 1, sizeof(readbuf), fp);
+			s = fwrite(readbuf, 1, r, fp_out);
+			if (r != s) {
+				LOGERR("Writing buffer to fp_out is not the same size as reading from file\n");
+				RETURN(-1);
+			}
+
+			total_size += s;
+		} while (r == sizeof(readbuf));
+
+		fflush(fp_out);
+		fclose(fp);
+	}
+
+	RETURN(total_size);
+}
+
+// child - child process... the main functionality of the child process resides here.
 void child(int fd)
 {
 	ENTER;
 	FILE *fp_in, *fp_out;
 	char buf[BUFFER_SIZE];
-	int received_get_request_for_index_html = 0;
-	FILE *fp;
 	char target_file[BUFFER_SIZE];
-	int received_get_request_for_target_file = 0;
+	int i, r;
+	int http_version;
+	struct split_string first_pass[MAX_SPLITS];
+	int first_pass_splits;
+	struct split_string second_pass[MAX_SPLITS];
+	int second_pass_splits;
 
 	fp_in = fdopen(fd, "r");
 	fp_out = fdopen(fd, "w");
 
 	if (!fp_in || !fp_out) {
+		LOGERR("Unable to use fdopen() to convert sockets to FILE *\n");
 		perror("fdopen");
 		RETURN();
 	}
@@ -155,41 +222,36 @@ void child(int fd)
 	if (fgets(buf, sizeof(buf), fp_in)) {
 		strip_newline(buf);
 		LOG("Received (first line): %s\n", buf);
-		{
-			struct split_string first_pass[MAX_SPLITS];
-			int first_pass_splits = split(buf, " ", first_pass, MAX_SPLITS);
 
-			LOGINFO("First pass: Splits = %d\n", first_pass_splits);
+		first_pass_splits = split(buf, " ", first_pass, MAX_SPLITS);
+		LOGINFO("First pass: Splits = %d\n", first_pass_splits);
 
-			if (first_pass_splits == 3) {
-				if (strcmp(first_pass[0].str, "GET") == 0) {
-					printf("Found GET!\n");
+		if ((first_pass_splits == 3) && (strcmp(first_pass[0].str, "GET") == 0)) {
+			if (strcmp(first_pass[2].str, "HTTP/1.1") == 0) {
+				http_version = 1;
+			}
+			else if (strcmp(first_pass[2].str, "HTTP/1.0") == 0) {
+				http_version = 0;
+			}
+			else {
+				http_version = -1;
+			}
 
-					struct split_string second_pass[MAX_SPLITS];
-					int second_pass_splits = split(first_pass[1].str, "?", second_pass, MAX_SPLITS);
+			second_pass_splits = split(first_pass[1].str, "?", second_pass, MAX_SPLITS);
+			LOGINFO("Second pass: Splits = %d\n", second_pass_splits);
 
-					LOGINFO("Second pass: Splits = %d\n", second_pass_splits);
-
-					if (second_pass_splits > 0) {
-						if ((strcmp(second_pass[0].str, "/") == 0) || (strcmp(second_pass[0].str, "/index.htm") == 0) || (strcmp(second_pass[0].str, "/index.html") == 0)) {
-							if ((strcmp(first_pass[2].str, "HTTP/1.1") == 0) || (strcmp(first_pass[2].str, "HTTP/1.0") == 0)) {
-								LOG("Found HTTP request for index.html\n");
-								received_get_request_for_index_html = 1;
-							}
-						}
-						else {
-							my_strlcpy(target_file, second_pass[0].str, sizeof(target_file));
-							LOG("Requesting file: %s\n", target_file);
-							received_get_request_for_target_file = 1;
-						}
-
-						int i;
-
-						for (i = 1; i < second_pass_splits; i++) {
-							LOGINFO("Second pass split[%d]: ?%s\n", i, second_pass[i].str);	// we include the question mark -- to show that it was a URL parameter, since split() removes delimiters.
-						}
-					}
+			if (second_pass_splits > 0) {
+				if ((strcmp(second_pass[0].str, "/") == 0) || (strcmp(second_pass[0].str, "/index.htm") == 0) || (strcmp(second_pass[0].str, "/index.html") == 0)) {
+					LOG("Found HTTP request for index.html\n");
+					my_strlcpy(target_file, "/index.html", sizeof(target_file));
 				}
+				else {
+					my_strlcpy(target_file, second_pass[0].str, sizeof(target_file));
+					LOG("Requesting file: %s\n", target_file);
+				}
+			}
+			for (i = 1; i < second_pass_splits; i++) {
+				LOGINFO("Second pass split[%d]: ?%s\n", i, second_pass[i].str);	// we include the question mark -- to show that it was a URL parameter, since split() removes delimiters.
 			}
 		}
 	}
@@ -202,63 +264,17 @@ void child(int fd)
 			break;
 	}
 
-	LOGINFO("Sending...\n");
+	if (http_version >= 0) {
+		LOGINFO("Sending file: %s\n", target_file);
 
-	if (received_get_request_for_index_html) {
-		fp = fopen("index.html", "rb");
-		if (!fp) {
-			fprintf(fp_out, "HTTP/1.1 404 Not Found\n");
-			fprintf(fp_out, "Content-Type: text/html; charset=utf-8\n\n");
-			fprintf(fp_out, "<HTML><BODY>File not found</BODY></HTML>\n");
-			fflush(fp_out);
-			goto close_fd;
+		r = http_send_file(http_version, target_file, fp_out);
+		if (r >= 0) {
+			LOGINFO("Succeeded sending file.\n");
 		}
 		else {
-			fprintf(fp_out, "HTTP/1.1 200 OK\n");
-			fprintf(fp_out, "Content-Type: text/html; charset=utf-8\n\n");
-
-			while (fgets(buf, sizeof(buf), fp)) {
-				fprintf(fp_out, "%s", buf);
-			}
-
-			fflush(fp_out);
-			fclose(fp);
+			LOGINFO("Failed to send file.\n");
 		}
 	}
-	else if (received_get_request_for_target_file) {
-		fp = fopen(target_file, "rb");
-		if (!fp) {
-			fprintf(fp_out, "HTTP/1.1 404 Not Found\n");
-			fprintf(fp_out, "Content-Type: text/html; charset=utf-8\n\n");
-			fprintf(fp_out, "<HTML><BODY>File not found</BODY></HTML>\n");
-			fflush(fp_out);
-			goto close_fd;
-		}
-		else {
-			char readbuf[1024];
-			int r;
-			struct stat statbuf;
-
-			// we want to follow symlinks...
-			stat(target_file, &statbuf);
-
-			fprintf(fp_out, "HTTP/1.1 200 OK\n");
-//			fprintf(fp_out, "Content-Type: image/png\n\n");
-			fprintf(fp_out, "Content-Length: %ld\n\n", statbuf.st_size);
-
-			do {
-				r = fread(readbuf, 1, sizeof(readbuf), fp);
-				fwrite(readbuf, 1, r, fp_out);
-			} while (r == sizeof(readbuf));
-
-			fflush(fp_out);
-			fclose(fp);
-		}
-	}
-
-
-close_fd:
-	LOGINFO("Closing fd...\n");
 
 	fclose(fp_in);
 	fclose(fp_out);
@@ -277,6 +293,7 @@ int main(int argc, char **argv)
 	struct passwd *p_pwd;
 
 	srand(time(NULL));
+
 
 	if (argc < 2) {
 		printf("Syntax: <listen port>\n");
@@ -329,8 +346,8 @@ int main(int argc, char **argv)
 
 		now = time(NULL);
 		LOG("Connected IP: %s [%s]\n", inet_ntoa(server.sin_addr), strip_newline(ctime(&now)));
+		fflush(stdout);
 
-#ifndef NO_FORK
 		pid = fork();
 		if (pid < 0) { perror("fork"); exit(1); }
 
@@ -340,9 +357,11 @@ int main(int argc, char **argv)
 			main_canary = rand();
 #endif
 			alarm(10); // allow 10 seconds before terminating the connection / child process.
-			child(fd);
-			alarm(0);
-			exit(0);
+
+			child(fd); // main child process function.
+
+			alarm(0); // reset alarm to zero.
+			exit(0); // exit child process.
 		}
 		// parent
 		else {
@@ -350,9 +369,6 @@ int main(int argc, char **argv)
 			// clear up all the children.
 			while (waitpid(-1, NULL, WNOHANG) > 0) ;
 		}
-#else
-		child(fd);
-#endif
 
 		fflush(stdout);
 	}
